70d30e04117464da8aaa3ab565c6ad39a44d3fef
sskates1 <simonirv2@gmail.com>
2014-07-25 22:21:05 -0400

    Created primitive route.

--- app/models/clark_wright_savings.rb
+++ app/models/clark_wright_savings.rb
@@ -1,26 +1,13 @@
-class NearestNeighbor
+class ClarkWrightSavings
   attr_accessor :previous_locations
   def initialize(trip)
     @trip = trip
     @previous_locations = []
     @tour_primative = []
     @savings_list = []
-  end
-
-  def get_basic_route
     @start_location = @trip.all_locations[0]
-    trip_legs = @trip.trip_legs
-
-    trip_legs.each do |trip_leg|
-      if (trip_leg.leg.start_location == start_location ||
-        trip_leg.leg.end_location == start_location) &&
-        !trip_leg.leg.end_location.nil?
-
-        legs_times << trip_leg
-
-        end
-      end
-    end
+    @end_point_1 = Location.new
+    @end_point_2 = Location.new
   end
 
   def get_route_distance
@@ -32,16 +19,77 @@ class NearestNeighbor
   end
 
   def create_tour_primative
+    location1 = Location.new
+    location2 = Location.new
+    @trip.trip_legs.each do |trip_leg1|
+      if trip_leg1.leg.start_location == @start_location &&
+        !trip_leg1.leg.end_location.nil?
 
+        location1 = trip_leg1.leg.end_location
+
+      elsif trip_leg1.leg.end_location == @start_location
+
+        location1 = trip_leg1.leg.start_location
+      else
+        next
+      end
+
+      @trip.trip_legs.each do |trip_leg2|
+
+        # look only at the legs going to the start location
+        if trip_leg2.leg.start_location == @start_location &&
+          !trip_leg2.leg.end_location.nil?
+
+          location2 = trip_leg2.leg.end_location
+
+        elsif trip_leg2.leg.end_location == @start_location
+
+          location2 = trip_leg2.leg.start_location
+        else
+          next
+        end
+        @trip.trip_legs.each do |connection_leg|
+          if (connection_leg.leg.start_location == location1 &&
+            connection_leg.leg.end_location == location2) ||
+            (connection_leg.leg.start_location == location2 &&
+              connection_leg.leg.end_location == location1)
+
+            @savings_list << { location1: location1,
+                              location2: location2,
+                              trip_leg1: trip_leg1,
+                              trip_leg2: trip_leg2,
+                              connection: connection_leg }
+
+            break
+            # end loop once found the right connection
+          end
+          # end if connection leg
+        end
+        # end .each connection leg
+      end
+      # end .each trip_leg2
+    end
+    # end .each trip_leg1
+    if @savings_list.length > 0
+      initial_tour_primative = @savings_list.max_by do |savings|
+        savings[:trip_leg1].time + savings[:trip_leg2].time - savings[:connection].time
+      end
+      @end_point_1 = initial_tour_primative[:location1]
+      @end_point_2 = initial_tour_primative[:location2]
+      @tour_primative.push( initial_tour_primative[:trip_leg1],
+                            initial_tour_primative[:connection],
+                            initial_tour_primative[:trip_leg2] )
+      @previous_locations.push(@start_location,@end_point_1, @end_point_2)
+    end
   end
+  # end create_tour_primative
 
   def tour_endpoints
     #savings should be created relative to the tour endpoints
+    #update endpoints
   end
 
   def get_route
-    get_basic_route
-    @start_location
     create_tour_primative
     while @previous_locations.length < @trip.all_locations.length
       update_savings_list
